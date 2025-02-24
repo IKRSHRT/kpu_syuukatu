caa252ccf64fa8877b94afd8a96d7dddc567c339
IKRSHRT <141113048+IKRSHRT@users.noreply.github.com>
2025-02-24 21:06:28 +0900

    Update postcss.config.mjs
    修正

--- syukatu/postcss.config.mjs
+++ syukatu/postcss.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // 追加
   },
 };
-
-export default config;
