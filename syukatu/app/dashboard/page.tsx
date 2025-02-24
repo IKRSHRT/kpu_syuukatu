3d7abdf3c6c99d93b9026e991da00706bd14de9f
IKRSHRT <20122005@kaishi-pu.ac.jp>
2025-02-24 22:00:47 +0900

    Move supabase.ts to app

--- syukatu/app/dashboard/page.tsx
+++ syukatu/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabase';
+import { supabase } from "../../lib/supabase";
 import { useRouter } from 'next/navigation';
 import Header from "../../components/Header";
 import LoadingState from "../../components/LoadingState";
